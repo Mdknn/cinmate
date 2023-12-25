2dbd3e62d670172526ab9a00e0129bc2ff451d00
Mdknn <mdkaif123000@gmail.com>
2023-12-25 21:51:48 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,20 +7,20 @@ export const Footer=()=> {
 
 <footer className="bg-white  shadow  dark:bg-gray-800">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a hrel="https://flowbite.com/" className="hover:underline" rel= "noreferrer">Flowbite™</a>. All Rights Reserved.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href='www' rel= "noreferrer">Cinmate</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a hrel="" className="hover:underline me-4 md:me-6" rel= "noreferrer">About</a>
+            <a href='www'  className="hover:underline me-4 md:me-6" rel= "noreferrer">About</a>
         </li>
         <li>
-            <a hrel="" className="hover:underline me-4 md:me-6" rel= "noreferrer">Privacy Policy</a>
+            <a  href='www' className="hover:underline me-4 md:me-6" rel= "noreferrer">Privacy Policy</a>
         </li>
         <li>
-            <a hrel="" className="hover:underline me-4 md:me-6" rel= "noreferrer">Licensing</a>
+            <a href='www'  className="hover:underline me-4 md:me-6" rel= "noreferrer">Licensing</a>
         </li>
         <li>
-            <a hrel="" className="hover:underline" rel= "noreferrer">Contact</a>
+            <a  href='www' className="hover:underline" rel= "noreferrer">Contact</a>
         </li>
     </ul>
     </div>
