e1bd721e27439e160bb0dbc5ce3a7ad40a4cda9e
Mdknn <mdkaif123000@gmail.com>
2023-12-25 22:27:32 +0530

    netlify.toml

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {  NavLink,  useNavigate } from 'react-router-dom'
+import {  NavLink,   useNavigate } from 'react-router-dom'
 import logo from '../asssts/logo.png'
 
 export const Header=() =>{
